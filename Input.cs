c634565750dff6d92aa5c856a637ea5329a4c998
Cyril Iselin <cyril_iselin@hotmail.com>
2020-09-23 16:24:46 +0200

    Initial

---
+++ Input.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    internal class Input
+    {
+        //Load list of available Keyboard buttons
+        private static readonly Hashtable KeyTable = new Hashtable();
+
+        //Perform a check to see if a particular button is pressed.
+        public static bool KeyPressed(Keys key)
+        {
+            if (KeyTable[key] == null)
+            {
+                return false;
+            }
+
+            return (bool) KeyTable[key];
+        }
+
+        //Detect if a keyboard button is pressed
+        public static void ChangeState(Keys key, bool state)
+        {
+            KeyTable[key] = state;
+        }
+    }
+}
